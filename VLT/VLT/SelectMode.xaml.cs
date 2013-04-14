0897193977dd6b6599c6bc319d2495bb52ba47c4
Adam Suczewski <asuczewski@gmail.com>
2013-04-14 14:16:55 -0400

    me and matt's ui with kinect in there kinda

---
+++ VLT/VLT/SelectMode.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VLT
+{
+    /// <summary>
+    /// Interaction logic for SelectMode.xaml
+    /// </summary>
+    public partial class SelectMode : Page
+    {
+        public SelectMode()
+        {
+            InitializeComponent();
+
+        }
+
+        private void quickLift_Click(object sender, RoutedEventArgs e)
+        {
+            quickLiftPage quickLiftPage = new quickLiftPage();
+            this.NavigationService.Navigate(quickLiftPage);
+            quickLiftPage.exerciseName.Content = this.exerciseName.Text;
+
+        }
+
+        private void teachMe_Click(object sender, RoutedEventArgs e)
+        {
+            teachMePage teachMePage = new teachMePage();
+            this.NavigationService.Navigate(teachMePage);
+            teachMePage.exerciseName.Content = this.exerciseName.Text;
+        }
+    }
+}
---
+++ VLT/VLT/quickLiftPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VLT
+{
+    /// <summary>
+    /// Interaction logic for quickLiftPage.xaml
+    /// </summary>
+    public partial class quickLiftPage : Page
+    {
+        private int currentSet = 1;
+        private int currentRep = 0;
+        public quickLiftPage()
+        {
+            InitializeComponent();
+        }
+        public void makeRep() {
+            Label setLabel;
+            //RowDefinition newSet;
+            if (currentRep == 0) {
+                // Make set row
+                Console.WriteLine("Making new set row");
+                setLabel = new Label();
+                setLabel.Content = "Set " + currentSet;
+                setLabel.Name = "Set" + currentSet;
+                setLabel.MouseLeftButtonDown += showData;
+                setRepList.Items.Add(setLabel);
+            }
+            currentRep++;
+
+            setLabel = new Label();
+            setLabel.Content = "\tRep " + currentRep;
+            setLabel.Name = "Rep" + currentRep;
+            setLabel.MouseLeftButtonDown += showData;
+            setRepList.Items.Add(setLabel);
+
+            Console.WriteLine("Increase current Rep: " + currentRep);
+        }
+        public void endSet()
+        {
+            currentRep = 0;
+            currentSet++;
+        }
+        public void repButtonClick(object sender, RoutedEventArgs e)
+        {
+            makeRep();
+        }
+
+        public void setButtonClick(object sender, RoutedEventArgs e)
+        {
+            endSet();
+        }
+        public void showData(object sender, RoutedEventArgs e)
+        {
+            issueBox.Items.Clear();
+            Label issueLabel = new Label();
+            issueLabel.Name = ((Label)sender).Name;
+            issueLabel.Content = ((Label)sender).Name;
+            issueLabel.MouseLeftButtonDown += showAdvice;
+
+            issueBox.Items.Add(issueLabel);
+
+        }
+        private int counter = 0;
+        public void showAdvice(object sender, RoutedEventArgs e)
+        {
+            counter++;
+            adviceBox.Text = counter.ToString();
+        }
+    }
+}
