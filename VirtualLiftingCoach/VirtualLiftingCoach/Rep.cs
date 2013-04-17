83f2cc52927588422e64fd71ec3f51c3db16870f
Andrew Callahan <acallaha@princeton.edu>
2013-04-17 19:48:11 -0400

    Added methods to score each rep
    Currently supports scoring knee width and depth in the back squat.

---
+++ VirtualLiftingCoach/VirtualLiftingCoach/Rep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VirtualLiftingCoach
+{
+    public class Rep
+    {
+        public string exercise;
+        public int[] scores;
+
+        public Rep()
+        {
+            scores = new int[2];
+            scores[0] = -1;
+            scores[1] = -1;
+        }
+    }
+}
